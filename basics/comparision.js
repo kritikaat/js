1ef2df26db61ca4ec8523792d0deb81f07107f31
krutika thakkar <134754200+kritikaat@users.noreply.github.com>
2024-01-02 06:20:48 +0000

    basics

---
+++ basics/comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//treue rest all will be false cause of while comparision null is converted to 0 for >=
+
+console.log(undefined == 0);
+console.log(undefined >= 0);//not for undefined 
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
